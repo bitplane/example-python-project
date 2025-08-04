c5613bb830a1891f6216c86f91ca61cd003d102e
Gareth Davidson <gaz@bitplane.net>
2025-08-04 05:16:14 +0100

    package names with hyphen in them

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -2,8 +2,10 @@
 
 source .venv/bin/activate
 
+package_name=$(echo $1 | tr '-' '_')
+
 # Run pytest with coverage reports and tee output
-pytest --cov="src/$1" --cov-report=html --cov-report=term-missing . 2>&1 | tee htmlcov/coverage_report.txt
+pytest --cov="src/$package_name" --cov-report=html --cov-report=term-missing . 2>&1 | tee htmlcov/coverage_report.txt
 
 # Extract just the missing coverage summary
 grep -A 20 "Missing" htmlcov/coverage_report.txt > htmlcov/missing_coverage.txt
